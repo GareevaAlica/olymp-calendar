b3c5f547b747528cc4f2f5ed264e0eda88ea2e39
Maxim Bordyugov <maxmutant1@yandex.ru>
2021-10-10 17:43:23 +0300

    parses web page and returns dict

--- app/utils/WebUtils.py
+++ app/utils/WebUtils.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+
 class WebUtils():
     @staticmethod
     def getHtmlByUrl(url):
@@ -17,8 +18,10 @@ class WebUtils():
         event_tokens = WebUtils.__getEventTokensFromHtml(htmlDoc)
         event_tokens.pop()
 
-        events = [event_tokens[i].contents[0].contents[0] for i in range(0, len(event_tokens), 2)]
-        event_deadlines = [event_tokens[i].contents[0] for i in range(1, len(event_tokens), 2)]
+        events = [event_tokens[i].contents[0].contents[0]
+                  for i in range(0, len(event_tokens), 2)]
+        event_deadlines = [event_tokens[i].contents[0]
+                           for i in range(1, len(event_tokens), 2)]
 
         for i in range(0, len(events)):
             eventToDeadline[events[i]] = event_deadlines[i]
@@ -28,5 +31,28 @@ class WebUtils():
     @staticmethod
     def __getEventTokensFromHtml(html):
         soup = BeautifulSoup(html, 'html.parser')
-        events = soup.findAll('a', attrs={'href': re.compile(r"/activity/.*/events/*")})
+        events = soup.findAll(
+            'a',
+            attrs={'href': re.compile(r"/activity/.*/events/*")}
+        )
         return events
+
+    @staticmethod
+    def getMapNameLink(url: str) -> dict:
+        res = dict()
+        soup = list(BeautifulSoup(
+            WebUtils.getHtmlByUrl(url),
+            'html.parser'
+        ).find_all('li'))[:-1]
+        for elem in soup:
+            if not isinstance(elem.contents[0], str):
+                res[elem.contents[0].contents[0]] = elem.contents[0]['href']
+            elif len(elem.contents) > 4:
+                res[elem.contents[0] + elem.contents[1].contents[0]] =\
+                    elem.contents[1]['href']
+                res[elem.contents[0] + elem.contents[3].contents[0]] =\
+                    elem.contents[3]['href']
+            else:
+                res[elem.contents[0]] = None
+        return res
+
