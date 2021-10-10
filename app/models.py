3dd0f7d803b10eb44ab05402279c98485395c802
elevankoff <elevankoff@yandex-team.ru>
2021-10-10 11:57:03 +0300

    Make seperate class for WebUtils

--- app/models.py
+++ app/models.py
@@ -1,8 +1,4 @@
 from app import db
-import requests
-from bs4 import BeautifulSoup
-import re
-from array import array
 
 # Файл для классов базы данных
 class User(db.Model):
@@ -16,32 +12,3 @@ class User(db.Model):
         db.session.add(self)
         db.session.commit()
         return self.id
-
-class WebUtils():
-    @staticmethod
-    def getHtmlByUrl(url):
-        response = requests.get(url)
-        return response.content
-
-    @staticmethod
-    def getEventsWithDeadlinesByUrl(url):
-        eventToDeadline = dict()
-
-        htmlDoc = WebUtils.getHtmlByUrl(url)
-
-        event_tokens = WebUtils.__getEventsFromHtml(htmlDoc)
-        events_tokens.pop()
-
-        events = [event_tokens[i].contents[0].contents[0] for i in range(0, len(event_tokens), 2)]
-        event_deadlines = [event_tokens[i].contents[0] for i in range(1, len(event_tokens), 2)]
-
-        for i in range(0, len(events)):
-            eventToDeadline[events[i]] = event_deadlines[i]
-
-        return eventToDeadline
-
-    @staticmethod
-    def __getEventTokensFromHtml(html):
-        soup = BeautifulSoup(html, 'html.parser')
-        events = soup.findAll('a', attrs={'href': re.compile(r"/activity/.*/events/*")})
-        return events
