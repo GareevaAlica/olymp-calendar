6f91c4ca0f6db16a80fba8b29914ac29957766c2
GareevaAlice <91534728+GareevaAlice@users.noreply.github.com>
2021-11-06 23:59:06 +0300

    Update fill_db.py

--- fill_db.py
+++ fill_db.py
@@ -3,12 +3,6 @@ from app import db
 from app.utils.DatabaseUpdater import DatabaseUpdater
 
 
-def create_user(user_email, calendar_id):
-    user = User(user_email=user_email, calendar_id=calendar_id)
-    user.save()
-    print(user)
-
-
 db.drop_all()  # удалить старые данные
 db.create_all()  # создать базу
 
@@ -20,10 +14,6 @@ db_updater = DatabaseUpdater()
 
 # получение уже собранной информации из /test_db/olympiads_info_list.json
 db_updater.save_olympiads_info_from_json()
-
-user_email = 'alica.gareeva@gmail.com'
-create_user(user_email,
-            'u54rqm90ph7enrmdu7tk2ol4l8@group.calendar.google.com')
 
 db.session.commit()  # обновляем базу
 print('Success')
